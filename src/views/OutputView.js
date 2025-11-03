58c0baaf4d8a1cb6fb6a55e5ed04233bf1995abb
eastar-DS <dororo0104@naver.com>
2025-11-03 19:46:16 +0900

    feat(OutputView): OutputView 생성 및 메소드 목록 작성

---
+++ src/views/OutputView.js
@@ -0,0 +1,13 @@
+import { Console } from '@woowacourse/mission-utils';
+
+class OutputView {
+  static printPurchaseCount(count) {}
+
+  static printLottos(lottos) {}
+
+  static printWinningStatistics(winningCountObject) {}
+
+  static printProfitRatio(profitRatio) {}
+}
+
+export default OutputView;

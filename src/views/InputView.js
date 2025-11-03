dd7847ee889ccf961646d29889a67d41ae3c75da
eastar-DS <dororo0104@naver.com>
2025-11-03 23:11:12 +0900

    refactor(InputView): 정의한 상수 적용

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,23 +1,26 @@
 import { Console } from '@woowacourse/mission-utils';
+import { ERROR_MESSAGES } from '../constants/errorMessages';
+import { LOTTO_CONFIG } from '../constants/lottoConfig';
+import { MESSAGES } from '../constants/messages';
 
 class InputView {
   static async readCost() {
-    const costString = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+    const costString = await Console.readLineAsync(MESSAGES.INPUT_PROMPT.COST);
     const cost = Number(costString.trim());
     if (Number.isNaN(cost)) {
-      throw new Error('[ERROR] 유효한 숫자를 입력해 주세요.');
+      throw new Error(ERROR_MESSAGES.INPUT.INVALID_COST_TYPE);
     }
-    if (cost < 1000) {
-      throw new Error('[ERROR] 1,000원 이상의 금액을 입력해주세요.');
+    if (cost < LOTTO_CONFIG.TICKET_PRICE) {
+      throw new Error(ERROR_MESSAGES.INPUT.INVALID_COST_MINIMUM);
     }
-    if (cost % 1000 > 0) {
-      throw new Error('[ERROR] 금액은 1,000원 단위로 입력해주세요.');
+    if (cost % LOTTO_CONFIG.TICKET_PRICE > 0) {
+      throw new Error(ERROR_MESSAGES.INPUT.INVALID_COST_UNIT);
     }
     return cost;
   }
 
   static async readWinningNumbers() {
-    const winningNumbersString = await Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
+    const winningNumbersString = await Console.readLineAsync(MESSAGES.INPUT_PROMPT.WINNING_NUMBERS);
     return winningNumbersString
       .split(',')
       .filter((number) => number.length > 0)
@@ -25,7 +28,7 @@ class InputView {
   }
 
   static async readBonusNumber() {
-    const bonusNumberString = await Console.readLineAsync('보너스 번호를 입력해 주세요.\n');
+    const bonusNumberString = await Console.readLineAsync(MESSAGES.INPUT_PROMPT.BONUS_NUMBER);
     const bonusNumber = Number(bonusNumberString.trim());
     return bonusNumber;
   }

68a892bd3b87900ab9d633819b221c7b2301f048
eastar-DS <dororo0104@naver.com>
2026-01-08 11:40:12 +0900

    feat: 로또 관련 상수 정의
    - 로또 가격, 번호 개수, 번호 범위 상수
    - 등수별 상금 상수
    - 에러 메시지 접두사 상수

---
+++ src/constants/constants.js
@@ -0,0 +1,11 @@
+export const LOTTO_PRICE = 1000;
+export const LOTTO_NUMBER_COUNT = 6;
+export const MIN_LOTTO_NUMBER = 1;
+export const MAX_LOTTO_NUMBER = 45;
+export const ERROR_PREFIX = '[ERROR]';
+
+export const FIRST_PRIZE = 2000000000;
+export const SECOND_PRIZE = 30000000;
+export const THIRD_PRIZE = 1500000;
+export const FOURTH_PRIZE = 50000;
+export const FIFTH_PRIZE = 5000;
